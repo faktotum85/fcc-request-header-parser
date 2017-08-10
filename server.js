a9eef819e6794a044013f3aaa6b80b75a3a566e2
Simon Weller <s.meierbeck@gmail.com>
2017-08-10 17:28:23 +0200

    fixing route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.get('/api/whoami', function(req, res) {
+app.get('/', function(req, res) {
 
   const ipaddress = req.headers['x-forwarded-for'] || "";
   let language = req.headers['accept-language'] || "";
