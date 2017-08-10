5c9869bd689e35a563f8762504797bd3c8a2a13b
Simon Weller <s.meierbeck@gmail.com>
2017-08-10 17:24:23 +0200

    parsing out relevant header information

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  console.log(req.headers['user-agent']);
-  console.log(req.headers['accept-language']);
-  res.end(JSON.stringify(req.headers));
+
+  const ipaddress = req.headers['x-forwarded-for'] || "";
+  let language = req.headers['accept-language'] || "";
+  if (language) {
+    language = language.split(',')[0];
+  }
+  let software = req.headers['user-agent'] || "";
+  if (software) {
+    software = software.match(/\(([^\)]*)\)/)[1];
+  }
+  res.end(JSON.stringify({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  }));
 });
 
 app.listen(port, function () {
